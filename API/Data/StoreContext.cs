a7cb2beba0299a72edb15321a6e6dc08a8191b10
Ahmad Salehi <ahmad_etna@hotmail.com>
2021-11-19 15:09:07 +0330

    End of Section 2

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
